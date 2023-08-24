29a4602508af929c6ed0d446e2cd12bb88a3a750
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-24 10:14:48 -0700

    finalizazo

--- proyects/twitter-follow-card/src/App.jsx
+++ proyects/twitter-follow-card/src/App.jsx
@@ -1,15 +1,43 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
 export function App () {
   return(
-    <article className='App'>
-      <TwitterFollowCard userName ="midudev" initialIsFollowing={true}>
-        Miguel Ángel Durán
-      </TwitterFollowCard>
-      <TwitterFollowCard userName ="batman" initialIsFollowing={false}>
-        Batman
-      </TwitterFollowCard>
-    </article>
+    <section className='App'>
+      {
+        users.map(({ userName, name , isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
   )
 }
