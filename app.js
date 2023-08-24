a7ae9bfdbf4b071beacbc6efb40729376973a47c
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-24 12:28:52 -0700

    carpeta dist

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('proyects/twitter-follow-card'))
+app.use(express.static('proyects/twitter-follow-card/dist'))
 
 app.get('*', (req, res) => {
     res.redirect('/');
