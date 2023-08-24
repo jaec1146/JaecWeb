804ed70925cedb88dcdd362526ce7d852d500085
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-23 17:36:09 -0700

    haciendo twitter-follow-card

---
+++ proyects/hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+
+// eslint-disable-next-line react/prop-types
+export function TwitterFollowCard ({children, userName}){
+  const [isFollowing,setIsFollowing] = useState(false)
+
+  const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing ? 'tw-followCard-button is-following' : 'tw-followCard-button'
+  
+  const handleClick = () =>{
+    setIsFollowing(!isFollowing)
+  }
+  return(
+      <div className="box">
+        <article className="tw-followCard">
+          <header className="tw-followCard-header">
+            <img
+              className="tw-followCard-avatar"
+              alt = 'nombre de usuario'
+              src={`https://unavatar.io/${userName}`} />
+            <div className="tw-followCard-info">
+              <strong> {children} </strong>
+              <span className="tw-followCard-infoUserName"> @{userName} </span>
+            </div>
+          </header>
+        </article>
+
+        <aside>
+          <button className={buttonClassName} onClick={handleClick}>
+            {text}
+          </button>
+        </aside>
+      </div>
+    )
+}
