046428af5ef13ca1ac6a54df5a6cd402225e1c05
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-26 08:15:39 -0700

    purebas de conexión a pag web server 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('/proyects/tic-tac-toe/dist/index.html'))
+app.use(express.static('/proyects/tic-tac-toe/dist/'))
 
 app.get('*', (req, res) => {
     res.redirect('/');
