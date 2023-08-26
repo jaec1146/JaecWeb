fd205b07a3f40d14e67a7e6fd5e3c4bd716e30c7
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-26 10:25:31 -0700

    purebas de conexión a pag web server 8

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.redirect('/proyects/tic-tac-toe/dist');
 })
 
