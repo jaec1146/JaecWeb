5ae893519cf73f736dd5745ed226233c11d6da1d
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-26 11:06:44 -0700

    pruebas de conexión a pag web server 12

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.redirect('/');
 })
 
+app.get('/TwitterFollowCard', (req, res) => {
+    res.render('proyects/twitter-follow-card/dist');
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);   
 })
