c4509274da775df17cc1c58d16eb3e8864969686
jose angel esquivel cisneros <jaec1146@gmail.com>
2023-08-29 13:33:26 -0700

    prueba en FLO 3

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
 
-app.use(express.static('proyects/twitter-follow-card/dist'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.redirect('/proyects/twitter-follow-card/dist');
+    res.redirect('/');
 })
 
 app.get('/TwitterFollowCard', (req, res)=>{
     console.log('entre')
-    express.static('proyects/twitter-follow-card/dist');
+    res.static('./proyects/twitter-follow-card/dist');
 });
 
 app.get('/TicTacToe', (req, res) => {
-    res.render('/proyects/tic-tac-toe/dist/index.html');
+    res.render('/proyects/tic-tac-toe/dist/');
 })
 
 app.listen(port,()=>{
